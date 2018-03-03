d48698812fd5323e1c89fd04b8e9a8798190e937
mexicoraver83 <jose.castelo83@gmail.com>
2018-03-03 14:11:07 -0600

    Clase Python 03/03/18

---
+++ dictionario.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+#Author: jcastelo on 030318
+
+mi_diccionario_morelia = {'Jugador': "Rui Diaz", 'Estadio': "Morelos", 'Televisora': "Morelos_TV", 'Tecnico': "Roberto Hernandez", 'Patrocinador': "Huawei"}
+print 'Mi jugador es: ' + str(mi_diccionario_morelia['Jugador'])
+print 'Mi tecnico es: ' + str(mi_diccionario_morelia['Tecnico'])
