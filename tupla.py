9ef07b482280ac4209750bee99c6e4a94b74e8c4
mexicoraver83 <jose.castelo83@gmail.com>
2018-03-03 14:10:20 -0600

    Clase Python 03/03/18

---
+++ tupla.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+#Author: jcastelo on 240218
+
+first_tuple = ('uno', 4 , 'cinco' ,'test')
+print 'Mi tupla es: ' + str(first_tuple)
+print 'El segundo elemento es: ' + str(first_tuple[2])
