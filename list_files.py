96946082be2a2864929e22738643cc8067e1b3f2
mexicoraver83 <jose.castelo83@gmail.com>
2018-03-04 08:17:31 -0600

    Script to list files in user input durectory

---
+++ list_files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#Author: jcastelo - 04/03/2018
+
+# This script will list the files in a directory
+
+import os
+
+path = raw_input("Enter the desire path to evaluate: ")
+path = path.rstrip()
+#print path
+# Here we are listing the content in the directory
+
+for f in os.listdir(path):
+	print 'Here is the content: ', f
